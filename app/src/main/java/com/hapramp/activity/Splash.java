ada228bd674adede99cc32cb252feaf0ca963624
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-03-16 18:27:01 +0530

    REM: code cleanup

--- app/src/main/java/com/hapramp/activity/Splash.java
+++ app/src/main/java/com/hapramp/activity/Splash.java
@@ -4,14 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.hapramp.IndexTester;
-import com.hapramp.SearchManager;
-
-import java.util.ArrayList;
-
 public class Splash extends AppCompatActivity{
 
-    private SearchManager searchManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
