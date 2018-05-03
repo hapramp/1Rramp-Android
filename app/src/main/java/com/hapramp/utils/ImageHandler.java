8e95c6683fa9e279b9f9f18703f9f23cef09fc56
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-05-03 22:18:26 +0530

    FIX: image resized on scroll down

--- app/src/main/java/com/hapramp/utils/ImageHandler.java
+++ app/src/main/java/com/hapramp/utils/ImageHandler.java
@@ -10,8 +10,13 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.bumptech.glide.request.target.Target;
 import com.hapramp.R;
+
 
 /**
  * Created by Ankit on 12/17/2017.
@@ -21,17 +26,29 @@ public class ImageHandler {
 
     public static void load(Context context, ImageView target, String _uri) {
 
-       try {
+       Glide.with(context)
+               .load(_uri)
+               .listener(new RequestListener<String, GlideDrawable>() {
+                   @Override
+                   public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                       return false;
+                   }
 
-           target.layout(0,0,0,0);
-
-            Glide.with(context)
-                    .load(_uri)
-                    .diskCacheStrategy(DiskCacheStrategy.RESULT)
-                    .into(target);
-        }catch (Exception e){
-
-       }
+                   @Override
+                   public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                       GlideDrawableImageViewTarget glideTarget = (GlideDrawableImageViewTarget) target;
+                        ImageView iv = glideTarget.getView();
+                        int width = iv.getMeasuredWidth();
+                        int targetHeight = width * resource.getIntrinsicHeight() / resource.getIntrinsicWidth();
+                        if(iv.getLayoutParams().height != targetHeight) {
+                            iv.getLayoutParams().height = targetHeight;
+                            iv.requestLayout();
+                        }
+                        return false;
+                   }
+               })
+               .diskCacheStrategy(DiskCacheStrategy.RESULT)
+               .into(target);
 
     }
 
