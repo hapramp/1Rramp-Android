277462b01761ad23e5995ccf37e0e0603d83768b
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-05-13 10:02:43 +0530

    add properties file

--- app/src/main/java/com/hapramp/services/HapramapPushService.java
+++ app/src/main/java/com/hapramp/services/HapramapPushService.java
@@ -14,21 +14,10 @@ public class HapramapPushService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d("Firebase", "From: " + remoteMessage.getFrom());
         if (remoteMessage.getData().size() > 0) {
             Log.d("Firebase", "Message data payload: " + remoteMessage.getData());
-
         }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d("Firebase", "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
         NotificationHandler.handleNotification(remoteMessage,this);
-
     }
-
 }
