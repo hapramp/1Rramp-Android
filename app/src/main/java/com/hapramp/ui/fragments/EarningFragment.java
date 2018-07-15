c69cb0b6548ab0b6df0d4d4e0623a9ccce92e722
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-07-15 23:49:10 +0530

    add cryptocoinview for better exchange rate of SBD and STEEM

--- app/src/main/java/com/hapramp/ui/fragments/EarningFragment.java
+++ app/src/main/java/com/hapramp/ui/fragments/EarningFragment.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.hapramp.R;
 import com.hapramp.preferences.HaprampPreferenceManager;
 import com.hapramp.steem.UserProfileFetcher;
@@ -18,13 +21,48 @@ import com.hapramp.ui.activity.AccountHistoryActivity;
 import com.hapramp.utils.ImageHandler;
 import com.hapramp.utils.ReputationCalc;
 import com.hapramp.views.extraa.BubbleProgressBar;
+
 import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import hapramp.walletinfo.Wallet;
+import xute.cryptocoinview.CoinView;
+import xute.cryptocoinview.Coins;
 
 public class EarningFragment extends Fragment implements Wallet.UserAccountFieldsCallback, UserProfileFetcher.UserProfileFetchCallback {
+
+		@BindView(R.id.user_image)
+		ImageView userImage;
+		@BindView(R.id.username)
+		TextView username;
+		@BindView(R.id.user_fullname)
+		TextView userFullname;
+		@BindView(R.id.user_reputation)
+		TextView userReputation;
+		@BindView(R.id.user_fullname_container)
+		LinearLayout userFullnameContainer;
+		@BindView(R.id.see_history_btn)
+		TextView seeHistoryBtn;
+		@BindView(R.id.steem_icon)
+		ImageView steemIcon;
+		@BindView(R.id.divider1)
+		ImageView divider1;
+		@BindView(R.id.steem_power_icon)
+		ImageView steemPowerIcon;
+		@BindView(R.id.divider2)
+		ImageView divider2;
+		@BindView(R.id.steem_dollar_icon)
+		ImageView steemDollarIcon;
+		@BindView(R.id.divider3)
+		ImageView divider3;
+		@BindView(R.id.steem_saving_icon)
+		ImageView steemSavingIcon;
+		@BindView(R.id.divider4)
+		ImageView divider4;
+		@BindView(R.id.estimated_value)
+		ImageView estimatedValue;
 		@BindView(R.id.wallet_steem_tv)
 		TextView walletSteemTv;
 		@BindView(R.id.wallet_steem_power_tv)
@@ -45,28 +83,25 @@ public class EarningFragment extends Fragment implements Wallet.UserAccountField
 		BubbleProgressBar savingProgress;
 		@BindView(R.id.estimated_value_progress)
 		BubbleProgressBar estimatedValueProgress;
-		@BindView(R.id.user_image)
-		ImageView userImage;
-		@BindView(R.id.username)
-		TextView username;
-		@BindView(R.id.user_reputation)
-		TextView userReputation;
+		@BindView(R.id.infor_card)
+		CardView inforCard;
+		@BindView(R.id.sbd_rate)
+		CoinView sbdRateView;
 		@BindView(R.id.steem_rate)
-		TextView steemRate;
-		@BindView(R.id.steem_dollar_rate)
-		TextView steemDollarRate;
-		@BindView(R.id.user_fullname)
-		TextView userFullname;
-		@BindView(R.id.steem_power_icon)
-		ImageView steemPowerIcon;
-		@BindView(R.id.see_history_btn)
-		TextView seeHistoryBtn;
+		CoinView steemRate;
 		private Handler mHandler;
 		private Wallet wallet;
 		private Unbinder unbinder;
 		private String mUsername;
 		private Context mContext;
 		private UserProfileFetcher userProfileFetcher;
+		//------
+		private double steem;
+		private double sbd;
+		private double sp;
+		private double sbd_rate;
+		private double steem_rate;
+		//------
 		public static final String ARG_USERNAME = "username";
 
 		public EarningFragment() {
@@ -88,6 +123,20 @@ public class EarningFragment extends Fragment implements Wallet.UserAccountField
 						mUsername = HaprampPreferenceManager.getInstance().getCurrentSteemUsername();
 				}
 				unbinder = ButterKnife.bind(this, view);
+				steemRate.setCoinId(Coins.STEEM);
+				steemRate.setRateCallback(new CoinView.RateCallback() {
+						@Override
+						public void onRate(double rate) {
+								steem_rate = rate;
+						}
+				});
+				sbdRateView.setCoinId(Coins.SBD);
+				sbdRateView.setRateCallback(new CoinView.RateCallback() {
+						@Override
+						public void onRate(double rate) {
+								sbd_rate = rate;
+						}
+				});
 				fetchUserInfo();
 				fetchWalletInfo();
 				attachListener();
@@ -232,17 +281,6 @@ public class EarningFragment extends Fragment implements Wallet.UserAccountField
 
 		@Override
 		public void onUsdRates(final double sbd_usd, final double steem_usd) {
-				mHandler.post(new Runnable() {
-						@Override
-						public void run() {
-								try {
-										steemDollarRate.setText(String.format(" $ %s", sbd_usd));
-										steemRate.setText(String.format(" $ %s", steem_usd));
-								}
-								catch (Exception e) {
-								}
-						}
-				});
 		}
 
 		@Override
