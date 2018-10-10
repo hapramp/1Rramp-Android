f6d807dc7cd1728e6c5e70abec48e1348b723843
Ankit Kumar <ankit.kumar071460@gmail.com>
2018-10-10 23:36:29 +0530

    add ui for competition feed item and details page

--- app/src/main/java/com/hapramp/ui/activity/CompetitionCreatorActivity.java
+++ app/src/main/java/com/hapramp/ui/activity/CompetitionCreatorActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
---
+++ app/src/main/java/com/hapramp/ui/activity/CompetitionDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.hapramp.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hapramp.R;
+
+public class CompetitionDetailsActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_competition_details);
+  }
+}
